738118efaca733e50ad21bb9af888c8f578ea8bf
Sadiya-Mehreen <sadiyamehreenaiml@gmail.com>
2026-03-11 01:59:10 +0530

    Reformat server.js with consistent import style

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
-
 import adminRouter from './routes/adminRoute.js';
 import doctorRouter from './routes/doctorRoute.js';
 import userRouter from './routes/userRoute.js';
@@ -52,3 +50,4 @@ app.get('/test-db', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`));
+
